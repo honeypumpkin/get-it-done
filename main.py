f32fdf79013ce20bd3aa0e21d0f71a0c44315081
honeypumpkin <robin.avila88@gmail.com>
2017-06-20 10:12:13 -0500

    initial commit get it done

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, request, render_template
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+
+
+tasks = []
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+    return render_template('todos.html', title="Get it Done", tasks=tasks)
